b043d5e87ca963cefab8392eb4c3d9f7ba96f670
Yuechun Mao <maoyuechun@gmail.com>
2025-10-31 21:20:58 -0400

    Add function of checking if an input is a positive int number and its unit test.

--- misc/tests/test_prime_util.py
+++ misc/tests/test_prime_util.py
@@ -25,7 +25,17 @@ class PrimeNumberRelateTests(unittest.TestCase):
         factorsOf28 = get_prime_factors(28)
         self.assertEqual(factorsOf28, [2, 2, 7])
         factorsOf13 = get_prime_factors(13)
-        self.assertEqual(factorsOf13, [13   ])  # 13 is prime, so its only prime factor is itself   
+        self.assertEqual(factorsOf13, [13   ])  # 13 is prime, so its only prime factor is itself
+    
+    def test_isPositiveIntNumber(self):
+        from misc.prime_number_util import isPositiveIntNumber
+        resultPositive = isPositiveIntNumber("25")
+        self.assertTrue(resultPositive)
+        resultNegative = isPositiveIntNumber("-10")
+        self.assertFalse(resultNegative)
+        resultNonInt = isPositiveIntNumber("abc")
+        self.assertFalse(resultNonInt)
+        
 # end of class PrimeNumberRelateTests  
 if __name__ == '__main__':
     unittest.main()
